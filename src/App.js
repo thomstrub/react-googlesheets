e24eccba552c28c5c5f7e7f03b9e3fb65d9d68f7
Thomas Strub <thomstrub@gmail.com>
2021-02-21 10:47:53 -0800

    changes object oriented state setup to hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import CsvForm from './components/CsvForm/CsvForm'
 
 function App() {
 
+  const [state, setState] = useState({
+    name: "",
+    age: "",
+    salary: "",
+    hobby: ""
+  });
+
+  function changeHandeler(e){
+    setState({
+      ...state,
+      [e.target.name] : e.target.value
+    });
+  }
+
+  function submitHandler(e){
+    e.preventDefault();
+    console.log(state)
+  }
 
   return (
     <CsvForm />
