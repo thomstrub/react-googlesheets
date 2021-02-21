7697fc6787383d23a9111458fbc0e6e4e43e0161
Thomas Strub <thomstrub@gmail.com>
2021-02-21 11:02:37 -0800

    updates form

--- src/components/CsvForm/CsvForm.jsx
+++ src/components/CsvForm/CsvForm.jsx
@@ -4,25 +4,26 @@ import './CsvForm.css';
 
 export default function CsvForm({state, changeHandler, submitHandler}){
 
+    const {name, age, salary, hobby} = state;
     return(
         <Container fluid className="container">
         <Header as='h2'>React Google Sheets!</Header>
-        <Form className="form">
+        <Form className="form" onSubmit={submitHandler}>
           <Form.Field>
             <label>Name</label>
-            <input placeholder='Enter your name' />
+            <input placeholder='Enter your name' type="text" name="name" value={name} onChange={changeHandler}/>
           </Form.Field>
           <Form.Field>
             <label>Age</label>
-            <input placeholder='Enter your age' />
+            <input placeholder='Enter your age' type="number" name="age" value={age} onChange={changeHandler}/>
           </Form.Field>
           <Form.Field>
             <label>Salary</label>
-            <input placeholder='Enter your salary' />
+            <input placeholder='Enter your salary' type="number" name="salary" value={salary} onChange={changeHandler}/>
           </Form.Field>
           <Form.Field>
             <label>Hobby</label>
-            <input placeholder='Enter your hobby' />
+            <input placeholder='Enter your hobby' type="text" name="hobby" value={hobby} onChange={changeHandler}/>
           </Form.Field>
           
           <Button color="blue" type='submit'>Submit</Button>
